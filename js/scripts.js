9309fa4087fbcb8ab463c93e825b28b5dd1117ab
Brent Perry (DAL - ACCF) <burentoperii@gmail.com>
2018-04-04 13:34:24 -0500

    add final segments to be hidden/shown.

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,28 @@ $(document).ready(function() {
         $("#show-potato").toggle();
         $("#hide-potato").toggle();
     });
+
+    $("#show-bread").click(function() {
+        $("#bread").toggle();
+        $("#show-bread").toggle();
+        $("#hide-bread").toggle();
+    });
+
+    $("#hide-bread").click(function() {
+        $("#bread").toggle();
+        $("#show-bread").toggle();
+        $("#hide-bread").toggle();
+    });
+
+    $("#show-kale").click(function() {
+        $("#kale").toggle();
+        $("#show-kale").toggle();
+        $("#hide-kale").toggle();
+    });
+
+    $("#hide-kale").click(function() {
+        $("#kale").toggle();
+        $("#show-kale").toggle();
+        $("#hide-kale").toggle();
+    });
 });
