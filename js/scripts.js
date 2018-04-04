afa64e818cab385918ba79f3a8553ab00d15532d
Brent Perry (DAL - ACCF) <burentoperii@gmail.com>
2018-04-04 13:27:17 -0500

    complete potato section, hide/show functions.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#show-potato").click(function() {
+        $("#potato").toggle();
+        $("#show-potato").toggle();
+        $("#hide-potato").toggle();
+    });
+
+    $("#hide-potato").click(function() {
+        $("#potato").toggle();
+        $("#show-potato").toggle();
+        $("#hide-potato").toggle();
+    });
+});
